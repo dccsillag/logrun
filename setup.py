0c87867e4c8dfd8f6c728f97d2d2b05d087bfafb
Daniel Csillag <dccsillag@gmail.com>
2021-02-28 14:51:27 -0300

    Add code in setup.py to have long description

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+import os
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as file:
+    long_description = file.read()
+
 
 
 setup(
     name="exlog",
     version="0.1.0",
     description="A convenient experiment logging package for Python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/dccsillag/exlog",
     author="Daniel Csillag",
     author_email="dccsillag@gmail.com",
