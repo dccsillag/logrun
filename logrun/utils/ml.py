227a587c2b211be56c1ed299f0623c5b8c53e044
Daniel Csillag <dccsillag@gmail.com>
2021-03-01 09:48:20 -0300

    Add support for PyTorch and Tensorflow models in logrun.utils.ml

--- logrun/utils/ml.py
+++ logrun/utils/ml.py
@@ -2,13 +2,46 @@
 Experiment logging utilities for Machine Learning.
 """
 
-from logrun.internals import experiment
+from logrun.internals import experiment, Artifact
 
 
 __all__ = [
+    'PyTorchModel',
+    'TensorFlowModel',
     'add_metric',
     'add_pytorch_model',
+    'add_tensorflow_model',
 ]
+
+
+class PyTorchModel(Artifact):
+    def __init__(self, model):
+        self.model = model
+
+    @staticmethod
+    def read(path: str):
+        import torch
+
+        return torch.load(path)
+
+    def write(self, path: str) -> None:
+        import torch
+
+        torch.save(self.model, path)
+
+
+class TensorFlowModel(Artifact):
+    def __init__(self, model):
+        self.model = model
+
+    @staticmethod
+    def read(path: str):
+        import tensorflow as tf
+
+        return tf.keras.models.load_model(path)
+
+    def write(self, path: str):
+        self.model.save(path)
 
 
 def add_metric(metric_name, value):
@@ -24,5 +57,25 @@ def add_metric(metric_name, value):
     experiment.add_extra_key('metric:' + metric_name, float(value), overwrite=False)
 
 
-def add_pytorch_model(model):
-    pass # TODO
+def add_pytorch_model(model, key='trained_model'):
+    """
+    Add a trained PyTorch model `model` under key `key` (which defaults to `"trained_model"`) to the
+    extra keys to be logged.
+    """
+
+    if not isinstance(key, str):
+        raise TypeError("key must be 'str'")
+
+    experiment.add_extra_key('model:' + key, PyTorchModel(model), overwrite=True)
+
+
+def add_tensorflow_model(model, key='trained_model'):
+    """
+    Add a trained TensorFlow model `model` under key `key` (which defaults to `"trained_model"`) to
+    the extra keys to be logged.
+    """
+
+    if not isinstance(key, str):
+        raise TypeError("key must be 'str'")
+
+    experiment.add_extra_key('model:' + key, TensorFlowModel(model), overwrite=True)
